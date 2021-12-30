e94dab9ea4a556f62b36ba646bb57cb97081c9a2
Lukas Krickl <lukas@krickl.dev>
2021-12-30 17:11:54 +0100

    Added code coverage to tests

--- check.sh
+++ check.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 # this script simply runs the test pipeline
-mypy core cli test  --disallow-untyped-defs && pytest test/ -v && pylint core cli test
+mypy core cli test  --disallow-untyped-defs \
+&& pytest --cov=. test/ -v \
+&& pylint core cli test \
