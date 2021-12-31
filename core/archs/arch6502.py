2fc53007c7ec50b988fad4b3ca1f904695de16aa
Lukas Krickl <lukas@krickl.dev>
2021-12-31 11:37:23 +0100

    Refactored opcode decoder

--- core/archs/arch6502.py
+++ core/archs/arch6502.py
@@ -8,28 +8,75 @@ from ..numfmt import IntFmt
 from ..parser import Parser
 from ..reader import read_i8_le, read_i16_le, read_none
 
+# instruction encoding:
+# aaabbbcc;
+# aaa and cc bits determine opcode;
+# bbb bits determine addressing mode;
+
 
 class InstructionModes(Enum):
-    IMMEDIATE = 0
-    ZEROPAGE = 1
-    ZEROPAGEX = 2
-    ABSOLUTE = 3
-    ABSOLUTEX = 4
-    ABSOLUTEY = 5
-    INDIRECTX = 6
-    INDIRECTY = 7
+    IMMEDIATE = 1
+    ZEROPAGE = 2
+    ZEROPAGEX = 3
+    ABSOLUTE = 4
+    ABSOLUTEX = 5
+    ABSOLUTEY = 6
+    INDIRECTX = 7
+    INDIRECTY = 8
+    ACCUMULATOR = 9
+
+    def mask(self) -> int:
+        if self == self.IMMEDIATE:
+            return self._apply(0x69)
+        elif self == self.ZEROPAGE:
+            return self._apply(0x65)
+        elif self == self.ZEROPAGEX:
+            return self._apply(0x75)
+        elif self == self.ABSOLUTE:
+            return self._apply(0x6D)
+        elif self == self.ABSOLUTEX:
+            return self._apply(0x7D)
+        elif self == self.ABSOLUTEY:
+            return self._apply(0x79)
+        elif self == self.INDIRECTX:
+            return self._apply(0x61)
+        elif self == self.INDIRECTY:
+            return self._apply(0x71)
+        elif self == self.ACCUMULATOR:
+            return self._apply(0x0A)
+        else:
+            return 0x00
+
+    def _apply(self, unmasked: int) -> int:
+        # addressing mode mask
+        addr_mode_mask = 0b00011100
+        return unmasked & addr_mode_mask
 
 
 class Parser6502(Parser):
     def __init__(self) -> None:
         nodes: List[Node] = (
-            self._make_instruction("lda", self._make_load(0xA0))
-            + self._make_instruction("sta", self._make_store(0x80))
-            + self._make_instruction("adc", self._make_load(0x60))
-            + self._make_instruction("and", self._make_load(0x20))
+            self._make_instruction("lda", self._make_load(self._opcode(0xA9)))
+            + self._make_instruction(
+                "sta", self._make_store(self._opcode(0x85))
+            )
+            + self._make_instruction(
+                "adc", self._make_load(self._opcode(0x69))
+            )
+            + self._make_instruction(
+                "and", self._make_load(self._opcode(0x29))
+            )
+            + self._make_instruction(
+                "asl", self._make_logic(self._opcode(0x1E))
+            )
         )
 
         Parser.__init__(self, nodes)
+
+    def _opcode(self, unmasked: int) -> int:
+        # opcode mask
+        opcode_mask = 0b11100011
+        return unmasked & opcode_mask
 
     def _get_prefix(self, mode: IntFmt) -> str:
         if mode == IntFmt.HEX:
@@ -75,24 +122,41 @@ class Parser6502(Parser):
     def _make_comparator(self, opcode: int) -> Callable[[Context, int], bool]:
         return lambda ctx, i: i == opcode
 
-    def _make_load(self, high: int) -> Dict[InstructionModes, int]:
+    def _make_load(self, mask: int) -> Dict[InstructionModes, int]:
         """
         creates a load instruction based on common instruction modes
-        only supply hi nibble
         """
         return {
-            InstructionModes.IMMEDIATE: high | 0x09,
-        } | self._make_store(high)
+            InstructionModes.IMMEDIATE: mask
+            | InstructionModes.IMMEDIATE.mask(),
+        } | self._make_store(mask)
 
-    def _make_store(self, high: int) -> Dict[InstructionModes, int]:
+    def _make_store(self, mask: int) -> Dict[InstructionModes, int]:
         return {
-            InstructionModes.ZEROPAGE: high | 0x05,
-            InstructionModes.ZEROPAGEX: high | 0x15,
-            InstructionModes.ABSOLUTE: high | 0x0D,
-            InstructionModes.ABSOLUTEX: high | 0x1D,
-            InstructionModes.ABSOLUTEY: high | 0x19,
-            InstructionModes.INDIRECTX: high | 0x01,
-            InstructionModes.INDIRECTY: high | 0x11,
+            InstructionModes.ZEROPAGE: mask | InstructionModes.ZEROPAGE.mask(),
+            InstructionModes.ZEROPAGEX: mask
+            | InstructionModes.ZEROPAGEX.mask(),
+            InstructionModes.ABSOLUTE: mask | InstructionModes.ABSOLUTE.mask(),
+            InstructionModes.ABSOLUTEX: mask
+            | InstructionModes.ABSOLUTEX.mask(),
+            InstructionModes.ABSOLUTEY: mask
+            | InstructionModes.ABSOLUTEY.mask(),
+            InstructionModes.INDIRECTX: mask
+            | InstructionModes.INDIRECTX.mask(),
+            InstructionModes.INDIRECTY: mask
+            | InstructionModes.INDIRECTY.mask(),
+        }
+
+    def _make_logic(self, mask: int) -> Dict[InstructionModes, int]:
+        return {
+            InstructionModes.ACCUMULATOR: mask
+            | InstructionModes.ACCUMULATOR.mask(),
+            InstructionModes.ZEROPAGE: mask | InstructionModes.ZEROPAGE.mask(),
+            InstructionModes.ZEROPAGEX: mask
+            | InstructionModes.ZEROPAGEX.mask(),
+            InstructionModes.ABSOLUTE: mask | InstructionModes.ABSOLUTE.mask(),
+            InstructionModes.ABSOLUTEX: mask
+            | InstructionModes.ABSOLUTEX.mask(),
         }
 
     # helper to create instruction nodes for the most common
@@ -103,7 +167,17 @@ class Parser6502(Parser):
         nodes: List[Node] = []
 
         for mode, opcode in modes.items():
-            if mode == InstructionModes.IMMEDIATE:
+            if mode == InstructionModes.ACCUMULATOR:
+                nodes.append(
+                    Node(
+                        read_i8_le,
+                        [],
+                        self._make_comparator(opcode),
+                        lambda ctx, i: f"{name} A",
+                        [],
+                    )
+                )
+            elif mode == InstructionModes.IMMEDIATE:
                 nodes.append(
                     Node(
                         read_i8_le,
@@ -113,7 +187,7 @@ class Parser6502(Parser):
                         [self._read_i8_hex_node("#")],
                     )
                 )
-            if mode == InstructionModes.ZEROPAGE:
+            elif mode == InstructionModes.ZEROPAGE:
                 nodes.append(
                     Node(
                         read_i8_le,
@@ -123,7 +197,7 @@ class Parser6502(Parser):
                         [self._read_i8_hex_node()],
                     )
                 )
-            if mode == InstructionModes.ZEROPAGEX:
+            elif mode == InstructionModes.ZEROPAGEX:
                 nodes.append(
                     Node(
                         read_i8_le,
--- test/archs/test_arch6502.py
+++ test/archs/test_arch6502.py
@@ -40,7 +40,6 @@ class TestArch6502(unittest.TestCase):
 
         self.assertEqual(ctx.address, 0x613)
         self.assertNotEqual(result, None)
-        print(result)
         if result is not None:
             self.assertEqual(
                 result,
@@ -53,5 +52,43 @@ class TestArch6502(unittest.TestCase):
                     "    lda $0200, y",
                     "    lda ($ab, x)",
                     "    lda ($ab), y",
+                ],
+            )
+
+    def test_it_should_parse_logical_arch6502_modes(self) -> None:
+        parser = Parser6502()
+        ctx = Context(0x600)
+        result = parser.parse(
+            ctx,
+            Binary(
+                bytes(
+                    [
+                        0x0A,
+                        0x06,
+                        0x44,
+                        0x16,
+                        0x44,
+                        0x0E,
+                        0x00,
+                        0x44,
+                        0x1E,
+                        0x00,
+                        0x44,
+                    ]
+                )
+            ),
+        )
+
+        self.assertEqual(ctx.address, 0x60B)
+        self.assertNotEqual(result, None)
+        if result is not None:
+            self.assertEqual(
+                result,
+                [
+                    "    asl A",
+                    "    asl $44",
+                    "    asl $44, x",
+                    "    asl $4400",
+                    "    asl $4400, x",
                 ],
             )
