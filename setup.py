05b735c727113991758544bcb410152c9d9f449f
Lukas Krickl <lukas@krickl.dev>
2022-01-01 13:21:12 +0100

    Updated package build

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup
 setup(
     name="yadasm",
     version="0.1.0",
-    packages=["cli", "core"],
+    packages=["cli", "core", "."],
+    package_data={"": ["README.md", "LICENSE"]},
     author="Lukas Krickl",
     author_email="lukas@krickl.dev",
     url="https://github.com/unlink2/yadasm",
     description="",
+    platforms=["any"],
     license="MIT",
+    scripts=["yadasm.py"],
 )
