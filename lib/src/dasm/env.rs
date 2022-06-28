bb7a3b7464642fb480ad4c47ac226ff73f2bdf57
Lukas Krickl <lukas@krickl.dev>
2022-06-28 21:48:52 +0200

    Added def

--- lib/src/dasm/env.rs
+++ lib/src/dasm/env.rs
@@ -1,1 +1,17 @@
-pub struct Env {}
+use std::collections::BTreeMap;
+
+pub type Address = u64;
+
+/// Recursive env for the parser to use as a label lookup
+pub struct Env {
+    defs: BTreeMap<Address, Def>,
+    labels: BTreeMap<Address, Def>,
+
+    // parent env
+    parent: Option<Box<Env>>,
+}
+
+/// A general purpose definition
+pub struct Def {
+    name: String,
+}
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,5 +1,5 @@
-#![cfg_attr(feature = "no_std", no_std)]
-#![cfg_attr(feature = "no_std", feature(alloc))]
+//#![cfg_attr(feature = "no_std", no_std)]
+//#![cfg_attr(feature = "no_std", feature(alloc))]
 pub mod dasm;
 
 pub mod error;
